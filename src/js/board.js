6b4b5e8b69bcc4730ea00b80ce9660e8c5de919a
Richard <thedibaccle@gmail.com>
2019-08-15 12:49:19 -0700

    replaced fill_board with update_board, added insert_at_separation_index, finished make clone

--- src/js/board.js
+++ src/js/board.js
@@ -42,9 +42,10 @@ function in_bounds(row, col) {
 
 
 function Board(size, p1, p2) {
-	this.board = this.fill_board(this.init_board(size));
+	this.board = this.init_board(size);
 	this.p1 = p1;
 	this.p2 = p2;
+	this.pieces_separator = 8;
 	this.pieces = this.init_pieces(size, p1, p2);
 }
 
@@ -64,73 +65,61 @@ Board.prototype.init_pieces = function(size) {
 		white_pieces.push({player: this.p1, cloned: false, row: 7, col: 1, removed: false});
 		black_pieces.push({player: this.p2, cloned: false, row: 0, col: 1, removed: false});
 	}
+		this.update_board(); //add pieces to board
     return white_pieces.concat(black_pieces);
 }
 
-Board.prototype.fill_board = function(board) {
-	let size = board.length;
-	for(let i=0; i<size; i++) {
-		board[7][i] = i;			//white pieces
-		board[0][i] = 8 + i;	//black pieces
+Board.prototype.update_board = function () {
+	this.board.map(row => row.map((cell, j) =>
+		cell != null ? (this.pieces[cell].removed ? null : this.pieces[cell]) : null ));
+}
+
+//Calls every time a clone is made
+Board.prototype.insert_at_seperation_index = function() {
+	for(let pi=this.pieces_separator; pi<this.pieces.length; pi++) {
+		//Finds index that seperates p1 and p2 pieces
+		if(this.pieces[pi].player != this.p1) {
+			this.pieces_separator = pi; //update
+			return pi;
+		}
 	}
-	return board;
 }
 
-Board.prototype.update = function () {
-	for(let [i, row] of this.board.entries()) {
-		//if board[pieces[cell].row][pieces[cell].col] == and !pieces[cell].removed
-
-		//if piece[cell] at different location than on board
-		row.map((cell, j) => {
-			let p = cell != null ? this.pieces[cell] : null;
-			if(this.board[p.row][p.col] == this.board[i][j])
-		};
-	}
-};
-
-/*
-Board.prototype.clone_piece(p) {
-	p.isCloned = true;
-}
-
-
-Board.prototype.is_this_jump = function(piece, row) {
-	return Math.abs(this.pieces[piece].row - row) === 2;
-}
-*/
-
-
-Board.prototype.make_clone = function(p, row, col) {
-	this.pieces[p].cloned = true;
-	//TODO: UPDATE BOARD function. get piece index from html? idk lol
-
+Board.prototype.make_clone = function(pi, row, col) {
+	this.pieces[pi].cloned = true;
 	//row will only be 0 or 7, so we can use this to determine player and placement
-	return (row) ? this.pieces.unshift({player: this.p1, cloned: true, row: row, col: col, removed: false})
-	 						 : this.pieces.push({player: this.p2, cloned: true, row: row, col: col, removed: false});
+	let player = row ? this.p1, this.p2;
+	let clone = {player: player, cloned: true, row: row, col: col, removed: false};
+	return this.pieces.splice(this.insert_at_seperation_index(),0, clone);
 }
 
-Board.prototype.can_clone = function(piece_i) {
-	let p = this.pieces[piece_i];
+Board.prototype.can_clone = function(pi) {
+	let p = this.pieces[pi];
 	return (!p.cloned && (p.player == this.p1 && !p.row) || (p.player == this.p2 && p.row == 7));
 }
 
-Board.prototype.get_player = function(piece_i) {
-	return this.pieces[piece_i].player;
+Board.prototype.get_player = function(pi) {
+	return this.pieces[pi].player;
+}
+
+Board.prototype.can_continue = function(pi) {
+	let moves = this.get_moves(pi);
+	return !moves.every(t => t == [] || t == {} || t = null);
 }
 
 //Player has no more moves when (1): all player's pieces are removed (2): every piece has no moves
 Board.prototype.has_moves = function(player) {
-	for(let i=0; i < this.pieces.length) {
-		let piece = this.pieces[i];
-		if(!piece.removed && piece.player == player) {
-			if(!this.get_moves(i).every(t => t == [] || t == {} || t = null)) return true;
+	for(let pi=0; pi < this.pieces.length; pi++) {
+		let p = this.pieces[pi];
+		if(!p.removed && p.player == player) {
+			if(this.can_continue(pi)) return true;
 		}
 	}
 	return false;
 }
 
-Board.prototype.valid_move = function(piece_i, row, col) {
-	let m = this.get_moves(piece_i);
+Board.prototype.valid_move = function(pi, row, col) {
+	let m = this.get_moves(pi);
 	let moves = [];
 	moves = moves.concat(m.phase, m.adjs, m.jumps, m.leaps);
 		for (let type in moves) {
@@ -141,20 +130,18 @@ Board.prototype.valid_move = function(piece_i, row, col) {
 	return false;
 }
 
-Board.prototype.can_continue = function(piece_i) {
-	let moves = this.get_moves(piece_i);
-	return !moves.every(t => t == [] || t == {} || t = null);
-}
 
-Board.prototype.do_move = function(p, row, col) {
-	let c = this.pieces[p];
+
+Board.prototype.do_move = function(pi, row, col) {
+	let c = this.pieces[pi];
 	this.board[c.row][c.col] = null;
 	c.row = row;
 	c.col = col;
-	this.board[row][col] = p;
+	this.board[row][col] = pi;
 
 	//TODO: if there is a captured piece, remove from this.board and this.pieces[capt_piece].removed = true
 
+	//let captured = this.pieces[c.captured_pi]
 
 
 	//check if
@@ -170,12 +157,12 @@ Board.prototype.do_move = function(p, row, col) {
 
 }
 
-Board.prototype.get_moves = function(piece) {
+Board.prototype.get_moves = function(pi) {
 	let adjs = [];
 	let phase = {};
 	let jumps = [];
 	let leaps = [];
-	let p = this.pieces[piece];
+	let p = this.pieces[pi];
 
 
 	if(cell_type(p.row, p.col) > 1 && !this.board[7 - p.row][7 - p.col]) phase = {row: 7 - p.row, col: 7 - p.col};
@@ -194,14 +181,14 @@ Board.prototype.get_moves = function(piece) {
 
 					if((phase_adj || phase_far) && !(phase_adj && phase_far)) {
 						let capt = phase_adj ? phase_adj : phase_far;
-						leaps.push({row: 7 - p.row, col: 7 - p.col, captured: capt});
+						leaps.push({row: 7 - p.row, col: 7 - p.col, captured_pi: capt});
 					}
 				}
 
 				if(cell_adj) {	//if adj cell occupied, jump_cell in bounds, jump_cell clear, and jump_cell has enemy piece
 					if(in_bounds(p.row + r*2, p.col + c*2) && !this.board[p.row + r*2][p.col + c*2]
 					&& this.get_player(cell_adj) != p.player) {
-							jumps.push({row: row  + r*2, col: p.col + c*2, captured: cell_adj});
+							jumps.push({row: row  + r*2, col: p.col + c*2, captured_pi: cell_adj});
 					}
 				} else adjs.push({row: row + r, col: col + c});	//adjacent moves
 			}
