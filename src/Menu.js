d99cca4ed6f3da59a0ee300f724f91da44e71f5d
Richard <thedibaccle@gmail.com>
2019-12-09 15:01:21 -0800

    added settings container, navigation container

--- src/Menu.js
+++ src/Menu.js
@@ -1,6 +1,42 @@
 class Menu extends React.Component {
   render() {
+    //Animate <Intro> on start up, fade in rest of Menu
     return (<NavContainer/>)
+  }
+}
+
+class SettingsContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { activeItem: '' };
+
+    let settingsItems: {
+        'sfx'   :{text: 'SFX', type:'toggle', crossed: False},
+        'music' :{text: 'Music', type:'toggle', crossed: False},
+        'ads'   :{text: 'Ads', type:'page'}
+      };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(activeItem) {
+    //Check if toggle, Ads, or background color change?
+    return e => {
+      e.preventDefault();
+      //this.setState({ activeItem });  //perform action in render (other than crossing icon or not)
+      if(settingsItems[activeItem].type == 'toggle') {
+        settingsItems[activeItem].crossed = !settingsItems[activeItem].crossed;
+      }
+    }
+  }
+
+  render() {
+    const settingsItems = settingsItems.values().map(item => <ToggleItem item={ item } handleClick = { this.handleClick }/>)
+    return (
+      <div className='settings-container'>
+          { settingsItems }
+      </div>
+    )
   }
 }
 
@@ -21,7 +57,6 @@ class NavContainer extends React.Component {
 
     this.handleClick = this.handleClick.bind(this);
   }
-
   handleClick(activeItem) {
     return e => {
       e.preventDefault();
@@ -46,8 +81,19 @@ class NavContainer extends React.Component {
 }
 
 function NavItem(props) {
-  return (
-    //TODO: css for icon
-    <div className='nav-item' id={ props.item.text } onClick={ props.handleClick(props.item.text) }></div>
-  )
+  let id = props.item.text.toLowerCase();
+  return (<div className='nav-item'
+               id={ id }
+               onClick={ props.handleClick(id) }></div>)
+}
+
+function SettingsItem(props) {
+  let id = props.item.text.toLowerCase();
+  let classes = 'settings-item';
+  if(props.item.type == 'toggle'){
+    classes += props.item.crossed ? '-crossed' : '';
+  }
+  return (<div className= { classes } //settings-item-crossed
+               id={ id }
+               onClick={ props.handleClick(id) }></div>)
 }
