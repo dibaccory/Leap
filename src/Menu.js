079ecd3aea100f9a7eaaef6056bb5177c2335282
Richard <thedibaccle@gmail.com>
2019-12-10 15:15:15 -0800

    Set up Navigation and Setting menus

--- src/Menu.js
+++ src/Menu.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import './css/menu.css';
+import './lib/fa/css/all.min.css';
 import NavigationModal from './NavigationModal';
 
 class Menu extends React.Component {
   constructor() {
     super();
 
-    this.state = {
-      activeNavItem: '',
+    this.state = { activeNavItem: '' };
+
+    this.openNavModal = this.openNavModal.bind(this);
     }
 
-    this.openNavigationModal = this.openNavigationModal.bind(this);
-    }
-
-    openNavigationModal(activeItem) {
-      return e => this.state.setState({activeNavItem: activeItem});
+    openNavModal(activeItem) {
+      if (activeItem !== this.state.activeNavItem) this.setState({activeNavItem: activeItem});
     }
 
   render() {
     //Animate <Intro> on start up, fade in rest of Menu
 
     return (
-      <div class='menu-bg'>
-        <div class='menu-title'>Leap</div>
+      <div className='menu'>
+        <div className='menu-title'>Leap</div>
         <SettingsContainer/>
-        <NavigationModal nav = { this.state.activeNavItem }/>
-        <NavigationContainer openNavigationModal = { this.openNavigationModal }/>
+        <NavigationModal activeNavItem = { this.state.activeNavItem }/>
+        <NavigationContainer openNavModal = { this.openNavModal }/>
       </div>
     )
   }
@@ -35,34 +34,38 @@ class SettingsContainer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { activeItem: '' };
+    this.state = { activeItem: '', condition: false };
 
     this.settingsItems= {
-        'sfx'   :{text: 'SFX', type:'toggle', crossed: false},
-        'music' :{text: 'Music', type:'toggle', crossed: false},
-        'ads'   :{text: 'Ads', type:'page'}
+        'sfx'   :{text: 'SFX', icon: 'fas fa-volume-down', type:'toggle', crossed: false},
+        'music' :{text: 'Music', icon: 'fas fa-music', type:'toggle', crossed: false},
+        'ads'   :{text: 'Ads', icon: 'fas fa-ad', type:'page'}
       };
 
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(activeItem) {
-    //Check if toggle, Ads, or background color change?
-    return e => {
-      e.preventDefault();
-      //this.setState({ activeItem });  //perform action in render (other than crossing icon or not)
-      if(this.settingsItems[activeItem].type == 'toggle') {
+      if(this.settingsItems[activeItem].type === 'toggle') {
         this.settingsItems[activeItem].crossed = !this.settingsItems[activeItem].crossed;
       }
-    }
+  }
+
+  toggleSettings() {
+    this.setState({condition: !this.state.condition});
   }
 
   render() {
-    const settingsItems = Object.values(this.settingsItems).map(item => <SettingsItem item={ item } handleClick = { this.handleClick }/>)
-    return (
-      <div className='settings-container'>
+    const settingsItems = Object.values(this.settingsItems).map(
+      (item, i) => <SettingsItem key={ i }
+                                 item={ item }
+                                 handleClick = { this.handleClick }/> );
+    let containerClass = this.state.condition ? 'settings-container active' : 'settings-container';
+    return ( <div className='settings'>
+        <i className='fas fa-cog' onClick = { () => this.toggleSettings() } />
+        <div className= { containerClass }>
           { settingsItems }
-      </div>
-    )
+        </div>
+      </div> );
   }
 }
 
@@ -71,60 +74,47 @@ class NavigationContainer extends React.Component {
     super(props)
 
     this.state = {
-      activeItem: '',
+      activeNavItem: props.activeNavItem,
       activeItemPos: 0,
       activeItemColor: '',
       menuItems: [
-        {text: 'Stats'},
-        {text: 'Create'},
-        {text: 'Friends'}
+        {text: 'Stats', icon: 'fas fa-medal', active: false},
+        {text: 'Create', icon: 'fas fa-plus', ready: false, active: false},
+        {text: 'Friends', icon: 'fas fa-users', active: false}
       ],
-    }
+    };
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick(activeItem) {
-    return e => {
-      e.preventDefault();
-      /*
-      this.setState({
-        activeItem,
-        activeItemPos: document.getElementById(activeItem).offsetLeft,
-        activeItemColor: window.getComputedStyle(document.getElementById(activeItem)).getPropertyValue('background-color'),
-      });
-      */
-      this.props.openNavigationModal(activeItem);
-
-    }
+    this.openNavModal = props.openNavModal;
   }
 
   render() {
-    const navItems = this.state.menuItems.map(item => <NavItem item={ item } handleClick = { this.handleClick }/>)
-    return (
-      <div className='nav-container'>
-        <span className='nav-item--active' style={{left: this.state.activeItemPos, backgroundColor: this.state.activeItemColor}}/>
-          { navItems }
-      </div>
-    )
+    const navItems = this.state.menuItems.map(
+      (item, i) => <NavItem key={ i }
+                            item={ item }
+                            openNavModal = { this.openNavModal }/> );
+    return ( <div className='nav-container'> { navItems } </div> );
   }
 }
 
 function NavItem(props) {
   let id = props.item.text.toLowerCase();
-  return (<div className='nav-item'
-               id={ id }
-               onClick={ props.handleClick(id) }></div>)
+  let classes = 'nav-item';
+  classes += props.item.ready ? ' ready' : '';
+  classes += props.item.active ? ' active': '';
+  return ( <div className= { classes } id={ id } onClick={ () => props.openNavModal(id) }>
+      <i className= { props.item.icon } ></i>
+    </div> );
 }
 
 function SettingsItem(props) {
   let id = props.item.text.toLowerCase();
   let classes = 'settings-item';
-  if(props.item.type == 'toggle'){
-    classes += props.item.crossed ? '-crossed' : '';
+  if(props.item.type === 'toggle'){
+    classes += props.item.crossed ? ' crossed' : '';
   }
-  return (<div className= { classes } //settings-item-crossed
-               id={ id }
-               onClick={ props.handleClick(id) }></div>)
+  return ( <div className= { classes } id={ id } onClick={ () => props.handleClick(id) }>
+    <i className= { props.item.icon } ></i>
+  </div> );
 }
 
 export default Menu;
