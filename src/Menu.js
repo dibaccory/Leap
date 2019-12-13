44153258fd3a29ad8cfaf82075ee4e2c48189af1
Richard <thedibaccle@gmail.com>
2019-12-12 18:53:16 -0800

    animation working... now to determine if I should change the structure

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/menu.css';
 import './lib/fa/css/all.min.css';
 import NavigationModal from './NavigationModal';
+import { CSSTransition } from 'react-transition-group';
+import { Container } from 'react-bootstrap';
 
 class Menu extends React.Component {
   constructor() {
@@ -16,13 +18,17 @@ class Menu extends React.Component {
       if (activeItem !== this.state.activeNavItem) this.setState({activeNavItem: activeItem});
     }
 
+    componentDidUpdate(prevProps, prevState) {
+      console.log("hoollo");
+    }
+
   render() {
     //Animate <Intro> on start up, fade in rest of Menu
 
     return (
       <div className='menu'>
         <div className='menu-title'>Leap</div>
-        <SettingsContainer/>
+        <Settings/>
         <NavigationModal activeNavItem = { this.state.activeNavItem }/>
         <NavigationContainer openNavModal = { this.openNavModal }/>
       </div>
@@ -34,15 +40,13 @@ class SettingsContainer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { activeItem: '', condition: false };
+    this.state = { activeItem: ''};
 
-    this.settingsItems= {
-        'sfx'   :{text: 'SFX', icon: 'fas fa-volume-down', type:'toggle', crossed: false},
-        'music' :{text: 'Music', icon: 'fas fa-music', type:'toggle', crossed: false},
-        'ads'   :{text: 'Ads', icon: 'fas fa-ad', type:'page'}
-      };
+
 
     this.handleClick = this.handleClick.bind(this);
+    this.wrapperRef = React.createRef();
+
   }
   handleClick(activeItem) {
       if(this.settingsItems[activeItem].type === 'toggle') {
@@ -50,23 +54,52 @@ class SettingsContainer extends React.Component {
       }
   }
 
-  toggleSettings() {
-    this.setState({condition: !this.state.condition});
+  toggleContainer() {
+    this.wrapperRef.current.classList.toggle('active');
+    //this.setState({condition: !this.state.condition});
   }
+
 
   render() {
     const settingsItems = Object.values(this.settingsItems).map(
       (item, i) => <SettingsItem key={ i }
                                  item={ item }
                                  handleClick = { this.handleClick }/> );
-    let containerClass = this.state.condition ? 'settings-container active' : 'settings-container';
     return ( <div className='settings'>
-        <i className='fas fa-cog' onClick = { () => this.toggleSettings() } />
-        <div className= { containerClass }>
+        <i className='fas fa-cog' onClick = { () => this.toggleContainer() } />
+        <div className= 'settings-container' ref={ this.wrapperRef }>
           { settingsItems }
         </div>
       </div> );
   }
+}
+
+function Settings() {
+  const options= {
+      'sfx'   :{text: 'SFX', icon: 'fas fa-volume-down', type:'toggle', crossed: false},
+      'music' :{text: 'Music', icon: 'fas fa-music', type:'toggle', crossed: false},
+      'ads'   :{text: 'Ads', icon: 'fas fa-ad', type:'page'}
+    };
+  let containerToggle = false;
+  const [container, showContainer] = useState(false);
+  //useEffect(()=>containerToggle = !containerToggle);
+  const settingsItems = Object.values(options).map( (item, i) =>
+    <SettingsItem
+      key={ i }
+      item={ item }
+    />
+  );
+  return ( <div className='settings'>
+      <i className='fas fa-cog' onClick = { () => showContainer(!container) } />
+      <CSSTransition
+        in={container}
+        timeout={200}
+        classNames= 'settings-container'
+      >
+        <div className='setcon'>{settingsItems}</div>
+      </CSSTransition>
+    </div>
+  );
 }
 
 class NavigationContainer extends React.Component {
@@ -112,7 +145,7 @@ function SettingsItem(props) {
   if(props.item.type === 'toggle'){
     classes += props.item.crossed ? ' crossed' : '';
   }
-  return ( <div className= { classes } id={ id } onClick={ () => props.handleClick(id) }>
+  return ( <div className= { classes } id={ id } /*onClick={ () => props.handleClick(id) }*/>
     <i className= { props.item.icon } ></i>
   </div> );
 }
